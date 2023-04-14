7bec411ef528ae1fd7e3e22cf1ac134725d1c852
lambadya14 <lambadya421@gmail.com>
2023-04-14 18:09:51 +0900

    fix feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import { ToastContainer } from "react-bootstrap";
 import MovieDetail from "./pages/MovieDetail";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchedMovie from "./pages/SearchMovie";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/search" element={<SearchedMovie />} />
       </Routes>
       <ToastContainer />
     </BrowserRouter>
